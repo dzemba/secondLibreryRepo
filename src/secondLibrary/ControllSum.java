19f101dc8677c0d44936b6e5e8f5049e7583f353
dzemba <dzemba@op.pl>
2015-11-10 12:27:34 +0100

    Druga klasa

--- src/secondLibrary/ControllSum.java
+++ src/secondLibrary/ControllSum.java
@@ -1,10 +1,37 @@
 package secondLibrary;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+
 public class ControllSum {
+	
+  public static void CheckSum(String FileName) throws Exception {
+   
+    String datafile = "test.txt"; 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("dupa");
-	}
+    MessageDigest md = MessageDigest.getInstance("SHA1");
+    FileInputStream fis = new FileInputStream(datafile);
+    byte[] dataBytes = new byte[1024];
+    
+    int nread = 0; 
+    
+    while ((nread = fis.read(dataBytes)) != -1) {
+      md.update(dataBytes, 0, nread);
+    };
 
+    byte[] mdbytes = md.digest();
+   
+    //convert the byte to hex format
+    StringBuffer sb = new StringBuffer("");
+    for (int i = 0; i < mdbytes.length; i++) {
+    	sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+    }
+    
+    System.out.println("Digest(in hex format):: " + sb.toString());
+    
+  }
 }
